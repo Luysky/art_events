c7682a482479211cc1e5d62962d997a76dc9844b
Valentine Cotter <valentin.cotter@hes-so.ch>
2021-09-02 09:54:14 +0200

    Affichage bouton de tri - V2

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -3,13 +3,9 @@ import 'package:flutter/material.dart';
 AppBar header(context, {bool isAppTitle = true, String titleText = "text"}) {
   return AppBar(
 
-
-
-
     title: Align(alignment: Alignment.centerLeft,child: Text(
       
       isAppTitle ? titleText : 'ErrorAppBar',
-
 
       style: TextStyle(
         color: Theme.of(context).primaryColor,
@@ -20,7 +16,6 @@ AppBar header(context, {bool isAppTitle = true, String titleText = "text"}) {
     ),
     centerTitle: true,
     backgroundColor: Theme.of(context).backgroundColor,
-
 
     /*Affichage du bouton de tri*/
     actions: <Widget>[
@@ -37,22 +32,6 @@ AppBar header(context, {bool isAppTitle = true, String titleText = "text"}) {
           },
         )
     ],
-
-
-    /*<Widget>[
-      IconButton(
-        padding: EdgeInsets.fromLTRB(0, 0, 40, 0),
-        icon: Icon(
-
-          Icons.filter_list,
-          size: 30.0,
-          color: Colors.white,
-        ),
-        onPressed: () {
-        },
-      )
-    ]*/
-
   );
   
 }
