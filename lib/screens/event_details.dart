3f44bd70cb0cf5e4e665b2299dd72ef2f0a662fe
draguarMatth <50259605+draguarMatth@users.noreply.github.com>
2021-09-03 14:40:21 +0200

    mini change

--- lib/screens/event_details.dart
+++ lib/screens/event_details.dart
@@ -8,7 +8,7 @@ import 'package:flutter/material.dart';
 import '../dummy_users.dart';
 
 // Before  >> for us in this app
- Firestore firestore = Firestore();
+ Firestore firestore = Firestore.instance;
 // Actual with higher dependencies' version
  //FirebaseFirestore firestore = FirebaseFirestore.instance;
 
@@ -209,8 +209,8 @@ class ScreenArguments {
                   builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
 
                     if (snapshot.data == null) {
-                      //return circularProgress();
-                      return Text("no DATA here !!!");
+                      return circularProgress();
+                      // return Text("no DATA here !!!");
                     }
                     final List<Text> children = snapshot.data.documents
                         .map((doc) => Text(doc['username']))
